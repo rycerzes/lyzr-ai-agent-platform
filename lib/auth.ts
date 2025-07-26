8a45aaa636f0c119c606b8475b4581a75acacd42
swappy <59965507+rycerzes@users.noreply.github.com>
2025-07-26 21:57:25 +0530

    feat(auth): implement authentication client and server setup with email OTP support

---
+++ lib/auth.ts
@@ -0,0 +1,22 @@
+
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/drizzle";
+import * as schema from "@/db/schema";
+import { emailOTP } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  plugins: [
+    emailOTP({
+      overrideDefaultEmailVerification: true,
+      async sendVerificationOTP({ email, otp, type }) {
+        // TODO: Replace with your email sending logic
+        console.log(`Send OTP ${otp} to ${email} for ${type}`);
+      },
+    }),
+  ],
+});
